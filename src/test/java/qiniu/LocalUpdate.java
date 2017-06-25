921e502bd762e19e059b091a28fcd6e224aabb39
-A- <1157988103@qq.com>
2017-06-25 13:33:07 +0800

    七牛字节传输测试

--- src/test/java/qiniu/LocalUpdate.java
+++ src/test/java/qiniu/LocalUpdate.java
@@ -13,40 +13,35 @@ import com.qiniu.util.StringMap;
 /**
  * Created by nbsaw on 6/25/2017.
  */
-public class Test {
-    public static void main(String[] args) {
+public class LocalUpdate {
+    public static void main(String[] args) throws QiniuException {
         // 凭证生成
         String accessKey = "QDmYg322MuVo4vmYAWk06I160-q9xiWKFXtZI7O3";
         String secretKey = "i-iCBJNP4_e0X1HqaWEy4zIBNpHnYi77GCUy0QFa";
         String bucket = "miaohu";
+        String qiniuUrl = "http://os33nc36m.bkt.clouddn.com/";
+
         // 默认不指定key的情况下，以文件内容的hash值作为文件名
         String key = null;
         Auth auth = Auth.create(accessKey, secretKey);
+
         // 返回数据格式设置
         StringMap putPolicy = new StringMap();
         putPolicy.put("returnBody", "{\"key\":\"$(key)\",\"hash\":\"$(etag)\",\"bucket\":\"$(bucket)\",\"fsize\":$(fsize)}");
         long expireSeconds = 3600;
         String upToken = auth.uploadToken(bucket, null, expireSeconds, putPolicy);
+
         //构造一个带指定Zone对象的配置类
         // 此处为华东机房的配置
         Configuration cfg = new Configuration(Zone.zone0());
         UploadManager uploadManager = new UploadManager(cfg);
+
         // 上传本地文件
         String localFilePath = "C:\\Users\\nbsaw\\Desktop\\sakumoto.png";
-        try {
-            Response response = uploadManager.put(localFilePath, key, upToken);
-            //解析上传成功的结果
-            DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
-            System.out.println(putRet.key);
-            System.out.println(putRet.hash);
-        } catch (QiniuException ex) {
-            Response r = ex.response;
-            System.err.println(r.toString());
-            try {
-                System.err.println(r.bodyString());
-            } catch (QiniuException ex2) {
-                //ignore
-            }
-        }
+        Response response = uploadManager.put(localFilePath, key, upToken);
+
+        //解析上传成功的结果
+        DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+        System.out.println(qiniuUrl + putRet.hash);
     }
 }
