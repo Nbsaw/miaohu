679b108686394405b7990d349203a202b5dc0900
-A- <1157988103@qq.com>
2017-07-08 13:52:37 +0800

    暂时取消掉测试用例

--- src/test/java/server/ServerTest.java
+++ src/test/java/server/ServerTest.java
@@ -10,15 +10,15 @@ import java.util.Arrays;
  * Some Server Test .
  */
 public class ServerTest {
-    @Test
-    // 显示所有服务器里面注册过的Bean
-    public void showAllBeans(){
-        ApplicationContext ctx = SpringApplication.run(Application.class, "");
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
-    }
+//    @Test
+//    // 显示所有服务器里面注册过的Bean
+//    public void showAllBeans(){
+//        ApplicationContext ctx = SpringApplication.run(Application.class, "");
+//        System.out.println("Let's inspect the beans provided by Spring Boot:");
+//        String[] beanNames = ctx.getBeanDefinitionNames();
+//        Arrays.sort(beanNames);
+//        for (String beanName : beanNames) {
+//            System.out.println(beanName);
+//        }
+//    }
 }
