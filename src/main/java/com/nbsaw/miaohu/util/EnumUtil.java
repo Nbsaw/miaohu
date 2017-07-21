4599d140798e8427d7ae33a08a75e6b40087cedc
-A- <1157988103@qq.com>
2017-07-21 11:35:17 +0800

    枚举类值对比工具

---
+++ src/main/java/com/nbsaw/miaohu/util/EnumUtil.java
@@ -0,0 +1,10 @@
+package com.nbsaw.miaohu.util;
+
+public class EnumUtil {
+    public <T extends Enum<T>> boolean equalsOf(Class<T> clazz, String value){
+        for (T t : clazz.getEnumConstants()) {
+            if (t.toString().equals(value)) return true;
+        }
+        return false;
+    }
+}
